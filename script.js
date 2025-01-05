6e36f644d7214b85ae0ac4f958f6c35fafbc8346
Evel <yraakayra@gmail.com>
2025-01-05 12:45:01 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,7 @@
+ // Ініціалізація каруселі
+ document.addEventListener('DOMContentLoaded', function () {
+    const carousel = document.querySelector('.carousel');
+    if (carousel) {
+        // Реалізуйте логіку для ручного налаштування каруселі
+    }
+});
